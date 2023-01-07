b34d329183f8aef614bdf5700eaf71ea4f78f02f
Sofia <orzhekhovska_ak16@nuwm.edu.ua>
2023-01-07 20:52:46 +0200

    second commit

--- searching.py
+++ searching.py
@@ -1,2 +1,6 @@
 def search(dataset, parameters):
-    pass
+    if parameters == 'sf'
+        first_name = input()
+        for key in dataset:
+            if dataset[key]['first_name'] == first_name:
+                print()
